d82cba681ccfce0f051c3b7601fefb7fd7e4e22d
gopal gupta <g.guptagopal007@gmail.com>
2026-06-10 07:52:02 +0530

    Update hello.py
    Added a comment explaining the purpose of the Hello World program.

--- hello.py
+++ hello.py
@@ -1,22 +1,2 @@
-# Python Practice
-Welcome to my Python learning repository.
-## AboutMe
-
-  I am Gopal Gupta, a Computer Teacher and CBSE IT Author.
-
-  Currently Learning :
-Python
-GitHub
-Artificial Intellegence
-Data Analytics
-
-## Goals
-
-Learn Python Programming
-Build Real Projects
-Create a Professional Portfolio
-Prepare for Tech Internship
-
-## Repository Contents
-
-hello.py
+#Prints a simple "Hello World" message.
+print("Hello ","World")
